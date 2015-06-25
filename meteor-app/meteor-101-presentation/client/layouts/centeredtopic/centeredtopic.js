cb1dc41d390f8c72d749a6fa8be8dd5950a1175b
Carsten Winsnes <carsten_15@hotmail.com>
2015-06-25 16:46:08 -0700

    presentation is broken

--- meteor-app/meteor-101-presentation/client/layouts/centeredtopic/centeredtopic.js
+++
@@ -1,6 +0,0 @@
-Template.CenteredTopic.transitionOptions = function(from, to, element) {
-  return {
-    with: "right-to-left",
-    extra: "fadeIn"
-  }
-}
--- meteor-app/meteor-101-presentation/client/layouts/topwbullets/topwbullets.js
+++
@@ -1,6 +0,0 @@
-Template.topwbullets.transitionOptions = function(from, to, element) {
-  return {
-    with: "right-to-left",
-    extra: "fadeIn"
-  }
-}
