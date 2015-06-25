6f224607620dcd27164df03daad8f795a3de9458
Paul Laskowski <pelaskowski@gmail.com>
2015-06-25 16:04:01 -0700

    enabled notifyclient

--- meteor-app/meteor101survey/client/app.jsx
+++ meteor-app/meteor101survey/client/app.jsx
@@ -7,6 +7,8 @@ Meteor.startup(function () {
   Session.set('deviceId', myId);
   Meteor.call('setUserId', myId);
 
+  NotifyClient.register();
+
   React.render(<SuggestionInputList />, document.getElementById('suggestion-box'));
   React.render(<SurveyList />, document.getElementById('survey-questions'));
 });
