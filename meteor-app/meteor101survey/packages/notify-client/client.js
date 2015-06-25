116bbab3d2837e59a8ae383694c7406c94f71d7a
Paul Laskowski <pelaskowski@gmail.com>
2015-06-25 16:28:30 -0700

    debug logging added for notifyclient package

--- meteor-app/meteor101survey/packages/notify-client/client.js
+++ meteor-app/meteor101survey/packages/notify-client/client.js
@@ -20,8 +20,8 @@ NotifyClient.register = function() {
 
 NotifyClient.sendToAllOtherClients = function(message) {
   Meteor.call('notifyOtherClients', NotifyClient.clientSessionId, message, function(error, result) {
-    //console.log("Error: " + error);
-    //console.log("Result: " + result);
+    console.log("Error: " + error);
+    console.log("Result: " + result);
   });
 };
 
